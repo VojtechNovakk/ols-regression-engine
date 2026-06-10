2b4a6d5afaacae4cd1494adb6f8ffda9771ca9cc
VojtechNovak <vojta.novak2005@gmail.com>
2026-06-10 16:21:32 +0200

    Implement column norm and dot product methods in Matrix

--- Matrix.hpp
+++ Matrix.hpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <vector>
 #include <cstddef>
+#include <cmath>
 
 class Matrix {
 private:
@@ -21,14 +22,37 @@ public:
     }
     size_t rows() const { return m_Rows; }
     size_t cols() const { return m_Cols; }
-    double& operator()(size_t r, size_t c) {
+    double& operator()(const size_t r, const size_t c) {
         if (r >= m_Rows || c >= m_Cols)
             throw std::out_of_range("Cannot find value with this index!");
         return m_Values[m_Cols*r+c];
     }
-    const double& operator()(size_t r, size_t c) const{
+    const double& operator()(const size_t r, const size_t c) const{
         if (r >= m_Rows || c >= m_Cols)
             throw std::out_of_range("Cannot find value with this index!");
         return m_Values[m_Cols*r+c];
+    }
+    double columnNormalization(const size_t c) const {
+        if (c >= m_Cols)
+            throw std::out_of_range("Cannot find column with this index!");
+        double sum = dotColumns(c, c);
+        return std::sqrt(sum);
+    }
+
+    double dotColumns(const size_t c1, const Matrix& other, const size_t c2) const {
+        if (c1 >= m_Cols || c2 >= other.m_Cols)
+            throw std::out_of_range("Cannot find columns with this index!");
+        if (m_Rows != other.m_Rows)
+            throw std::logic_error("Cannot do this operation with Matrixes of differrent sizes!");
+        double sum = 0.0;
+        for (size_t r = 0; r < m_Rows; ++r)
+            sum += (*this)(r, c1) *  other(r, c2);
+        return sum;
+    }
+
+    double dotColumns(const size_t c1, const size_t c2) const {
+        if (c1 >= m_Cols || c2 >= m_Cols)
+            throw std::out_of_range("Cannot find columns with this index!");
+        return dotColumns(c1, *this, c2);
     }
 };
--- test.cpp
+++ test.cpp
@@ -32,7 +32,7 @@ bool testMatrixValues() {
     Matrix m1(3,4);
     m1(0,0) = 5.4;
     m1(1,2) = 1.3;
-    UNIT_TEST(std::abs(m1(0,0)-5.0) < EPSILON, "Value on m1(0, 0) is 5.4!");
+    UNIT_TEST(std::abs(m1(0,0)-5.4) < EPSILON, "Value on m1(0, 0) is 5.4!");
     UNIT_TEST(std::abs(m1(1,2)-1.3) < EPSILON, "Value on m1(1, 2) is 1.3!");
     Matrix m2(m1);
     m2(0,0) = 2.2;
@@ -52,6 +52,53 @@ bool testMatrixBounds() {
     return true;
 }
 
+bool testDot() {
+    Matrix m1(3,2);
+    m1(0,0) = 1.0;
+    m1(1,0) = 2.0;
+    m1(2,0) = 4.0;
+    m1(0,1) = -3.0;
+    m1(1,1) = 0.0;
+    m1(2,1) = 1.0;
+    Matrix m2(m1);
+
+    UNIT_TEST(std::abs(m1.dotColumns(0, 0) - 21) <= EPSILON, "This operation should be 21!");
+    UNIT_TEST(std::abs(m1.dotColumns(1,1) - 10) <= EPSILON, "This operation should be 10!");
+    UNIT_TEST(std::abs(m1.dotColumns(0,1) - 1) <= EPSILON, "This operation should be 1!");
+    UNIT_TEST(std::abs(m1.dotColumns(0, m2, 1) - 1) <= EPSILON, "This operation should be 1!");
+    try {
+        m1.columnNormalization(2);
+        UNIT_TEST(false, "There should be std::out_of_order error!");
+    }catch (const std::out_of_range& e) {}
+    try {
+        m1.columnNormalization(-1);
+        UNIT_TEST(false, "There should be std::out_of_order error!");
+    }catch (const std::out_of_range& e) {}
+    return true;
+}
+
+bool testColumnNormalization() {
+    Matrix m1(3,2);
+    m1(0,0) = 1.0;
+    m1(1,0) = 2.0;
+    m1(2,0) = 4.0;
+    m1(0,1) = -3.0;
+    m1(1,1) = 0.0;
+    m1(2,1) = 1.0;
+
+    UNIT_TEST(std::abs(m1.columnNormalization(0) - std::sqrt(21)) <= EPSILON, "This normalization should be sqrt(21)!");
+    UNIT_TEST(std::abs(m1.columnNormalization(1) - std::sqrt(10)) <= EPSILON, "This normalization should be sqrt(10)!");
+    try {
+        m1.columnNormalization(2);
+        UNIT_TEST(false, "There should be std::out_of_order error!");
+    }catch (const std::out_of_range& e) {}
+    try {
+        m1.columnNormalization(-1);
+        UNIT_TEST(false, "There should be std::out_of_order error!");
+    }catch (const std::out_of_range& e) {}
+    return true;
+}
+
 void runAllTests() {
     std::cout << "Running tests!\n";
 
@@ -68,6 +115,8 @@ void runAllTests() {
     runTest(testMatrixInit, "Init Matrix");
     runTest(testMatrixValues, "Matrix Values");
     runTest(testMatrixBounds, "Matrix Bounds");
+    runTest(testDot, "Matrix Dot Operation");
+    runTest(testColumnNormalization, "Column Normalization");
 
     std::cout << "Results: " << passed << " / " << total << "\n";
 }
